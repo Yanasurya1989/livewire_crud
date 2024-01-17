09ae0490eae301a10e13b0cbc9da62d41217ae56
YanaWebProgrammer <teukita15@gmail.com>
2024-01-17 14:34:13 +0700

    First commit

---
+++ resources/views/livewire/create.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form>
+        <div class="form-group mb-3">
+            <label for="categoryName">Name:</label>
+            <input type="text" class="form-control @error('name') is-invalid @enderror" id="categoryName" placeholder="Enter Name" wire:model="name">
+            @error('name') <span class="text-danger">{{ $message }}</span>@enderror
+        </div>
+        <div class="form-group mb-3">
+            <label for="categoryDescription">Description:</label>
+            <textarea class="form-control @error('description') is-invalid @enderror" id="categoryDescription" wire:model="description" placeholder="Enter Description"></textarea>
+            @error('description') <span class="text-danger">{{ $message }}</span>@enderror
+        </div>
+        <div class="d-grid gap-2">
+            <button wire:click.prevent="store()" class="btn btn-success btn-block">Save</button>
+        </div>
+    </form>
+</body>
+</html>
